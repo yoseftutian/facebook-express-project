941e5dbddd6415f5becee15f1704292e5928fcb7
yonatan.r <y.rahman770770@gmail.com>
2024-05-06 22:31:00 +0300

    I connected the MongoDB database
    I connected the MongoDB database

--- index.mjs
+++ index.mjs
@@ -2,10 +2,14 @@ import express from'express'
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send(err.message);
+  });
+  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
