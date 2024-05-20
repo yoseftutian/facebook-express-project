dc02c5b5e0c3db7ff4feca5bd9c849ef4c8ec938
yosef-tutian <yoseftutian@gmail.com>
2024-05-20 14:27:05 +0300

    add request

--- index.mjs
+++ index.mjs
@@ -1,13 +1,12 @@
-import express from'express'
+import express, { json } from "express";
 import Products from "./products.mjs";
 import dotenv from "dotenv";
 import cors from "cors";
 
 dotenv.config();
 
-const app = express()
-const port = 3001
-
+const app = express();
+const port = 3005;
 
 app.use(json());
 app.use(cors());
@@ -15,10 +14,10 @@ app.use(cors());
 app.use("/products", Products);
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send(err.message);
-  });
-  
+  console.error(err.stack);
+  res.status(500).send(err.message);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
