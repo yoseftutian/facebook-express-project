08c7dc3eb358e04626f3f5905c991f57a0984a54
yosef-tutian <yoseftutian@gmail.com>
2024-05-26 11:41:09 +0300

    updat database.mjs

--- database.mjs
+++ database.mjs
@@ -18,22 +18,17 @@ export const client = new MongoClient(mongoURI, {
     deprecationErrors: true,
   },
 });
+let cluster;
 
-let db;
-
-async function connectToDatabase() {
-  try {
-    const cluster = await client.connect();
-    db = cluster.db("facebook");
-    console.log("Connected to MongoDB");
-  } catch (e) {
-    console.error("Failed to connect to MongoDB", e);
-    throw e;
-  }
+try {
+  cluster = await client.connect();
+  console.log("Connected to MongoDB");
+} catch (e) {
+  console.error("Failed to connect to MongoDB", e);
+  throw e;
 }
 
-await connectToDatabase();
-
+const db = cluster.db("facebook");
 export const productsCollection = db.collection("products");
 export const postsCollection = db.collection("posts");
 
