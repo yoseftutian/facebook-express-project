76b080c14eb46db8e29a969d9c566804c01e05d4
yosef-tutian <yoseftutian@gmail.com>
2024-06-02 15:56:11 +0300

    add get req in posts

--- freinds.mjs
+++ freinds.mjs
@@ -3,25 +3,42 @@ import { usersCollection } from "./database.mjs";
 import { ObjectId } from "mongodb";
 const router = Router();
 
-
-
+// router.get("/:_id", async (req, res, next) => {
+//   const userId = new ObjectId(req.params._id);
+//   try {
+//     const freindsId = await usersCollection.findOne(
+//       { _id: userId },
+//       { projection: { _id: 0, freinds: 1 } }
+//     );
+//     console.log(freindsId.freinds);
+//     if (!freindsId.freinds) throw new Error("No friends.");
+//     const freindsDeaitails = await usersCollection
+//       .find(
+//         { _id: { $in: freindsId.freinds } },
+//         { projection: { firstName: 1, lastName: 1, baverImg: 1 } }
+//       )
+//       .toArray();
+//     // console.log(freindsDeaitails);
+//     console.log(freindsDeaitails);
+//     res.status(200).send(freindsDeaitails);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 router.get("/:_id", async (req, res, next) => {
-    const userId = new ObjectId(req.params._id);
-    try {
-        const freindsId = await usersCollection.findOne({ _id: userId }, { projection: { _id: 0, freinds: 1 } });
-        console.log(freindsId.freinds);
-        if (!freindsId.freinds)
-            throw new Error("No friends.")
-        const freindsDeaitails = await usersCollection.find({ _id: { $in: freindsId.freinds } }, { projection: { firstName: 1, lastName: 1, baverImg: 1 } }).toArray();
-        // console.log(freindsDeaitails);
-        console.log(freindsDeaitails);
-        res.status(200).send(freindsDeaitails);
-    } catch (error) {
-        next(error);
-    }
+  const userId = new ObjectId(req.params._id);
+  try {
+    const user = await usersCollection.findOne({ _id: userId });
+    if (!user) throw new Error("No user found.");
+    const friends = await usersCollection
+      .find({ _id: { $in: user.freinds } })
+      // { projection: { firstName: 1, lastName: 1, baverImg: 1 } })
+      .toArray();
+    res.status(200).send(friends);
+  } catch (error) {
+    next(error);
+  }
 });
 
-
 export default router;
-
