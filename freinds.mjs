5a07c83692801fb04f60a18ccf079de3b90d6215
chaimsolo <145326116+chaimsolo@users.noreply.github.com>
2024-06-02 15:43:43 +0300

    add friend added

--- freinds.mjs
+++ freinds.mjs
@@ -23,5 +23,24 @@ router.get("/:_id", async (req, res, next) => {
 });
 
 
+router.post("/addFreind", async (req, res, next) => {
+    // const { uid, fid } = req.body;
+    const uid = new ObjectId(req.body.uid);
+    const fid = new ObjectId(req.body.fid);
+    
+    try {
+        const user1 = await usersCollection.updateOne({_id: uid}, {$addToSet: {freinds: fid}});
+        const user2 = await usersCollection.updateOne({_id: fid}, {$addToSet: {freinds: uid}});
+        console.log("user1:", user1, "user2:", user2);
+       
+        res.status(200).send(true);
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+
+
 export default router;
 
