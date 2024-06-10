a262fd22dda4677b878ab5ed9b77274d0ec01259
yosef-tutian <yoseftutian@gmail.com>
2024-06-10 09:49:56 +0300

    currentTime in creatPost

--- posts.mjs
+++ posts.mjs
@@ -35,9 +35,9 @@ router.post("/", async (req, res, next) => {
     session.startTransaction(); // Start the transaction
 
     const data = req.body; // Get the data from the request body
-    currentTime = new Date();
-    data["createdAt"] = format
-    
+    const currentTime = new Date();
+    data["createdAt"] = currentTime;
+
     const insertedPost = await postsCollection.insertOne(data, { session }); // Insert the new post into the postsCollection within the session
     await usersCollection.updateOne(
       { _id: new ObjectId(data.owner) }, // Find the user by owner ID
